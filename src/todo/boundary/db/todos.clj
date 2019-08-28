8018aaeb5b669d35d7c33fb945e92f1c6e9cb620
Kento Okamura <kentooooo.1230@gmail.com>
2019-08-29 04:00:29 +0900

    update,delete,fetch実装

--- src/todo/boundary/db/todos.clj
+++ src/todo/boundary/db/todos.clj
@@ -4,7 +4,10 @@
 
 (defprotocol Todos
   (get-todos [db])
-  (create-todo [db params]))
+  (fetch-todo [db id])
+  (create-todo [db params])
+  (update-todo [db id params])
+  (delete-todo [db id]))
 
 (extend-protocol Todos
   duct.database.sql.Boundary
@@ -12,7 +15,16 @@
   (get-todos [{:keys [spec]}]
     (jdbc/query spec ["SELECT * FROM todos"]))
 
+  (fetch-todo [{:keys [spec]} id]
+    (jdbc/query spec [(format "SELECT * FROM todos WHERE id = '%s'" id)]))
+
   (create-todo [{:keys [spec]} params]
     (jdbc/insert! spec :todos {:title (:title params)}))
+
+  (update-todo [{:keys [spec]} id params]
+    (jdbc/update! spec :todos {:title (:title params)} ["id = ?" id]))
+
+  (delete-todo [{:keys [spec]} id]
+    (jdbc/delete! spec :todos ["id = ?" id]))
 
   )
--- src/todo/handler/todos.clj
+++ src/todo/handler/todos.clj
@@ -8,7 +8,23 @@
     (let [todos (todos/get-todos db)]
       [::response/ok todos])))
 
+(defmethod ig/init-key ::fetch [_ {:keys [db]}]
+  (fn [{[_ id] :ataraxy/result}]
+    (let [todo (todos/fetch-todo db id)]
+      [::response/ok todo])))
+
 (defmethod ig/init-key ::create [_ {:keys [db]}]
   (fn [{[_ params] :ataraxy/result}]
-    (let [result (todos/create-todo db params)]
-      [::response/created])))
+    (let [result (todos/create-todo db params)
+          id (:id (first result))]
+      [::response/created (str "/todos/" id)])))
+
+(defmethod ig/init-key ::update [_ {:keys [db]}]
+  (fn [{[_ id params] :ataraxy/result}]
+    (todos/update-todo db id params)
+    [::response/no-content]))
+
+(defmethod ig/init-key ::delete [_ {:keys [db]}]
+  (fn [{[_ id] :ataraxy/result}]
+    (todos/delete-todo db id)
+    [::response/no-content]))
